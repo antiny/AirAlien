4af88caada9c14fc0a2a4676b10dffc8b3ab07e8
An Tran <tranminhan.my@gmail.com>
2016-02-21 15:17:49 +0700

    add user and style layout

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'sprockets-rails'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def avatar_url(user)
+		if user.image
+			return user.image
+		else
+			gravatar_id = Digest::MD5::hexdigest(user.email).downcase 
+			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=50"
+		end
+	end 
+
 end
