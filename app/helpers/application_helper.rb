070988e5633e5e118eb487ac7151cb48ad66507e
An Tran <tranminhan.my@gmail.com>
2016-02-24 16:08:35 +0700

    implement confirmation email with mandrill

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
 	def avatar_url(user)
-		if user.image
-			return user.image
-		else
-			gravatar_id = Digest::MD5::hexdigest(user.email).downcase 
-			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=50"
-		end
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase 
+		"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=50"
 	end 
 
 end
