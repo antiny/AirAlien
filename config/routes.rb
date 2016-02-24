b6a95807db9def56cf151157af783e8762cbfdce
An Tran <tranminhan.my@gmail.com>
2016-02-24 17:34:29 +0700

    add show user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   get 'pages/home'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  resources :users, only: [ :show ]
 
   
   # The priority is based upon order of creation: first created -> highest priority.
